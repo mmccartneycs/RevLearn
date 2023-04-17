e93d0798fe065b0afaf1b03ac3c1d68e3556749c
adrizik <rizik_adriana@hotmail.com>
2023-04-17 16:36:33 -0400

    buttons send http requests

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { HttpClientModule } from "@angular/common/http";
 import { FormsModule } from '@angular/forms';
+import { ReactiveFormsModule } from '@angular/forms'
 
 
 import { AppComponent } from './app.component';
@@ -57,6 +58,7 @@ const routes: Routes = [
     BrowserAnimationsModule,
     RouterModule.forRoot(routes),
     FormsModule,
+    ReactiveFormsModule,
 
     // Angular Material Imports
     MatCardModule,
--- frontend/src/app/services/account.service.ts
+++ frontend/src/app/services/account.service.ts
@@ -13,14 +13,14 @@ export class AccountService {
 
   ev = "http://localhost:9000"
 
-  postAccountAPI(account: Account) {
+  postLoginAPI(account: Account) {
     let header: HttpHeaders = new HttpHeaders();
     header.append("accept", "text/json");
     header.append("Access-Control-Allow-Origin", "*");
     return this.http.post<Account>(`${this.ev}/student`, account, { headers: header });
   }
 
-  getAccountAPI(account: Account) {
+  postRegisterAPI(account: Account) {
     let header: HttpHeaders = new HttpHeaders();
     header.append("accept", "text/json");
     header.append("Access-Control-Allow-Origin", "*");
