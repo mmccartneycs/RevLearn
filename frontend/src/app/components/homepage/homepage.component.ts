cf6c2c1e90232d80a8b0a17f35e3fd0670e009e3
adrizik <rizik_adriana@hotmail.com>
2023-04-19 12:19:35 -0400

    disable and enable buttons

--- frontend/src/app/components/homepage/homepage.component.ts
+++ frontend/src/app/components/homepage/homepage.component.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import { FormControl, Validators } from '@angular/forms';
 import { Account } from 'src/app/models/account';
 import { AccountService } from 'src/app/services/account.service';
+import { AuthService } from 'src/app/services/auth.service';
 
 @Component({
   selector: 'app-homepage',
@@ -15,7 +16,10 @@ export class HomepageComponent {
   reg_email: string = ""
   reg_password: string = ""
 
-  constructor(private accountService: AccountService) { }
+  message: string = "";
+  messageVisible: boolean = true;
+
+  constructor(private accountService: AccountService, private authService: AuthService) { }
 
 
   emailFormControl = new FormControl('', [Validators.required, Validators.email]);
@@ -25,14 +29,21 @@ export class HomepageComponent {
 
   postLogin(): void {
     let account: Account = { email: this.email, password: this.password }
-    this.accountService.postLoginAPI(account).subscribe((acc : Object) => {
-      console.log(acc);
-    });
+    this.accountService.postLoginAPI(account).subscribe(
+      (acc: any) => {
+        console.log(acc);
+        this.authService.isLoggedIn = true;
+      },
+      error => {
+        if (error.status === 401) {
+          this.message = "Invalid email or password. Please try again."
+        }
+      });
   }
 
   postRegister(): void {
     let account: Account = { email: this.reg_email, password: this.reg_password }
-    this.accountService.postRegisterAPI(account).subscribe((acc : Account) => {
+    this.accountService.postRegisterAPI(account).subscribe((acc: Account) => {
       console.log(acc);
     });
   }
